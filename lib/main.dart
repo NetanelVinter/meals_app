bfe7edbf2b5777b5d79cacc285c9559b96bc2db3
NetanelVinter <uogi100@gmail.com>
2023-05-15 18:51:03 +0300

    start

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+ColorScheme _colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 131, 57, 0),
+  brightness: Brightness.dark,
+);
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+
+  final _theme = ThemeData(
+    useMaterial3: true,
+    colorScheme: _colorScheme,
+    textTheme: GoogleFonts.latoTextTheme(),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _theme,
+      home: Scaffold(
+        body: Text('data'),
+      ),
+    );
+  }
+}
