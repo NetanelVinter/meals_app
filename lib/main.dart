ce78054323b773d61e677092419cb8e132f9b865
NetanelVinter <uogi100@gmail.com>
2023-05-15 20:22:44 +0300

    Adding GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/screens/category_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
-
-ColorScheme _colorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 131, 57, 0),
-  brightness: Brightness.dark,
-);
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-
   final _theme = ThemeData(
     useMaterial3: true,
-    colorScheme: _colorScheme,
-    textTheme: GoogleFonts.latoTextTheme(),    
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromARGB(255, 131, 57, 0),
+      brightness: Brightness.dark,
+    ),
+    textTheme: GoogleFonts.latoTextTheme(),
   );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: _theme,
-      home: const Scaffold(
-        body: Text('data'),
-      ),
+      home: const CategoryScreen(),
     );
   }
 }
